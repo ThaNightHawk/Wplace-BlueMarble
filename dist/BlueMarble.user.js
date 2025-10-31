38629fb0fe5deedd26b81971e051ad45bce37ebc
Hawk <hasfeldt@yahoo.dk>
2025-10-31 22:28:01 +0100

    Bump version to 0.69.9 in package.json, package-lock.json, BlueMarble.user.js, and BlueMarble.meta.js; update URLs to reflect new feature branch

--- dist/BlueMarble.user.js
+++ dist/BlueMarble.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Skirk Marble - Place.hawk.quest Edition
 // @namespace    https://github.com/Seris0/
-// @version      0.69.8
+// @version      0.69.9
 // @description  A userscript to automate and/or enhance the user experience on place.hawk.quest. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with place.hawk.quest in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Blue Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       Seris0
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/ThaNightHawk/Wplace-BlueMarble
-// @icon         https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/refs/heads/main/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @icon         https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/refs/heads/feat/place/dist/assets/Favicon.png
+// @updateURL    https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/feat/place/dist/BlueMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/feat/place/dist/BlueMarble.user.js
 // @run-at       document-start
 // @match        *://place.hawk.quest/*
 // @grant        GM_getResourceText
@@ -19,7 +19,7 @@
 // @grant        GM_xmlhttpRequest
 // @connect      nominatim.openstreetmap.org
 // @connect      place.hawk.quest
-// @resource     CSS-BM-File https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/refs/heads/main/dist/BlueMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/ThaNightHawk/Wplace-BlueMarble/refs/heads/feat/place/dist/BlueMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://place.hawk.quest
